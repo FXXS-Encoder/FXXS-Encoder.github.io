70484ad90a8e83a64738b29bac202319af69594b
FXXS <priston910423@gmail.com>
2025-09-11 15:03:04 +0800

    Site updated: 2025-09-11 15:03:03

--- Scrips/srt.py
+++ Scrips/srt.py
@@ -1,11 +1,12 @@
 #coding:utf-8
 ##https://felixc.at/2010/07/srt-multiline-convert-python/
+#字幕文件必须是utf-8
 import re
 import sys
-if len(sys.argv)<2: exit();
+if len(sys.argv)<2: sys.exit(0)
 filename = sys.argv[1]
 
-f=open(filename,'r')
+f=open(filename,'r', encoding="utf-8")
 a=f.read()
 
 aa=a.split('\n\n')
@@ -25,7 +26,7 @@ for a in aa:
     if len(eng)>0: k.append(" ".join(eng))
     k.append('')
 
-f=open(filename+"_fixed.srt","w")
+f=open(filename+"_fixed.srt","w", encoding="utf-8")
 for m in k:
     f.write(m +"\n")
 f.close()
